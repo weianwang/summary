cd2a8043a442c68b636249730d3342571edf7ca5
Weian Wang <weianwang95@gmail.com>
2015-09-19 09:12:11 -0400

    Added Article API functionality to get article text

--- popup.js
+++ popup.js
@@ -1,17 +1,3 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-var http = require('http');
-
-var options = {
-  chrome.tabs.query({'active': true, 'lastFocusedWinter': true}, function (tabs){
-    var url = tabs[0].url;
-  });
-  host: 'http://api.diffboth.com/v3/article?' + 'token=2bc180cbefa2ca3d7e384d6b98971bb0&url=' + url
-    + '&field=text';
-
-}
 /**
  * Get the current URL.
  *
@@ -47,6 +33,25 @@ function getCurrentTabUrl(callback) {
     callback(url);
   });
 
+ // HTTP + GET request to Article API
+var xmlHttp = null;
+
+function getArticleText(){
+	//I still don't quite understand callback functions
+	var url = getCurrentTabUrl(function(url));
+	var host = 'http://api.diffbot.com/v3/article?' + 'token=2bc180cbefa2ca3d7e384d6b98971bb0&url=' + url
+    + '&field=text';
+    xmlHttp = new XMLHttpRequest();
+    // I cannot for the life of me figure out what this does but it apprently has to do with asynchronous things.
+    xmlHttp.onreadystatechange = function(){
+    	if (xmlHttp.readySTate == 4 && xmlHttp.status == 200)
+    		callback(xmlHttp.responseText);
+    }
+    xmlHttp.open ( "GET", host, true);
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
   // Most methods of the Chrome extension APIs are asynchronous. This means that
   // you CANNOT do something like this:
   //
@@ -57,72 +62,7 @@ function getCurrentTabUrl(callback) {
   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
 
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
-  });
-});
